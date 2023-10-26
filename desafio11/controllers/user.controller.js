ea62f128127d308bb420a2a6ede3bce8c53642a8
Facundo Castellano <castellanofacundo@gmail.com>
2023-10-25 22:13:32 -0300

    se avanzo sobre el endpoint para disparar el mail, pero falta

--- desafio11/controllers/user.controller.js
+++ desafio11/controllers/user.controller.js
@@ -2,10 +2,26 @@ const { response } = require('express')
 const passport = require('passport')
 const { factoryManager } = require('../config/process.config')
 const { hashPassword } = require('../utils/password.utils')
+const { CustomError, ErrorType } = require('../errors/custom.error')
 
 const userManager = factoryManager.userManager
 class UserController {
-  static refreshPassword = async (req, res = response) => {
+  static sendMailToRefreshPassword = async (req, res = response, next) => {
+    try {
+      const email = req.body.email
+      res.send('enviando mail a:', email)
+    } catch (err) {
+      next(
+        new CustomError(
+          err.message,
+          ErrorType.OTRO,
+          'UserController-sendMailToRefreshPassword'
+        )
+      )
+    }
+  }
+
+  static refreshPassword = async (req, res = response, next) => {
     try {
       const id = req.user._id //lo saco del user que me da el token, para seguridad.
       const passwordNew = hashPassword(req.body.password)
@@ -16,7 +32,7 @@ class UserController {
       next(
         new CustomError(
           'No se pudo cambiar el password',
-          ErrorType.DB,
+          ErrorType.OTRO,
           'UserController-refreshPassword'
         )
       )
--- desafio11/routes/api/user.router.js
+++ desafio11/routes/api/user.router.js
@@ -1,12 +1,15 @@
 const { Router } = require('express')
 const UserController = require('../../controllers/user.controller')
-const isAuthToken =require("../../middelwares/userAuthToken")
+const isAuthToken = require('../../middelwares/userAuthToken')
 
 const router = Router() //este objeto contendra todas las rutas de esta seccion, es lo que al final exporto.
 
 // TODAS LAS RUTAS QUE SIGUEN tienen por defecto el prefijo "/api/users"
 
-router.post('/refresh-pass',isAuthToken, UserController.refreshPassword) //crea un carrito nuevo (pero tengo que asociarlo a un userId)
+//esta ruta es la que efectivamente hace el update del password
+router.post('/refresh-pass', isAuthToken, UserController.refreshPassword)
 
+//esta ruta es la que efectivamente hace el update del password
+router.post('/send-mail-refresh-pass', UserController.sendMailToRefreshPassword)
 
 module.exports = router
